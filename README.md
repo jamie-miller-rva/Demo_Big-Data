831252d4c64d4cda3c24012aa77bab35e7b7e950
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-02-08 22:53:34 -0500

    adding assignment resources folder

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Demo_Big-Data
 Demo Big-Data applications to include NLP
+
+## Background:
+
+The four Vs of big data are being used to determine when to migrate from regular data to big data solutions.
+
+* **Volume** refers to the size of data (e.g., terabytes of product information). For instance, a year's worth of stock market transactions is a large amount of data.
+* **Velocity** pertains to how quickly data comes in (customers across the world purchasing every second). As an example, McDonald's restaurants are worldwide with customers buying food at a constant rate, so the data comes in fast.
+* **Variety** relates to different forms of data (e.g., user account information, product details, etc.). Consider the breadth of Netflix user information, videos, photos for thumbnails, and so forth.
+* **Veracity** concerns the uncertainty of data (e.g., reviews might not be real and could come from bots). As an example, Netflix would want to verify whether users are actively watching the shows, falling asleep, or just playing them in the background.
+
+Note: HDFS - Hadoop Distributed File System (HDFS)
+
---
+++ module_work/Bacon_count.py
@@ -0,0 +1,36 @@
+# demo using mapreduce to create a sample text file and see if you can find the number of times different words appear.
+
+# note add MRJOB to PythonData environment using pip install MRJob
+
+# import dependencies
+from mrjob.job import MRJob
+
+# Note: a class contains all the instructions to create a unique object
+# Create a class called Bacon_count, which inherits, or takes properties, from the MRJob class.
+# We create this class to be called to run the full MapReduce job withMRJob:
+
+
+class Bacon_count(MRJob):
+
+    # Next, create a mapper()function that will take (self, _, line) as parameters.
+    # The mapper() function will assign the input to key-value pairs:
+    def mapper(self, _, line):
+
+        # The second parameter (here using an underscore (_), explained next) allows methods to be mapped together. Since we are not chaining anything together, we use the Python convention of an underscore to indicate that we won’t use this parameter.
+        # The line parameter will be the line of text taken from the raw input file.
+
+        # split out each word for each line and use yeild to "count" the occurances of bacon
+        for word in line.split():
+            if word.lower() == "bacon":
+                yield "bacon", 1
+                
+    # shuffle and reduce
+    def reducer(self, key, values):
+        yield key, sum(values)
+            
+# add conventional Python code for running the program:
+if __name__ == "__main__":
+    Bacon_count.run()
+
+
+
